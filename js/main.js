c214b431f4dfef5ce4f01d549c97fd0152569490
Matheus Lereno <matheuslerenodasilva@gmail.com>
2025-07-16 16:45:56 -0300

    Exercicio corrigido plugins jquery

--- js/main.js
+++ js/main.js
@@ -1,35 +1,41 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        arrows: false
+});
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
 })
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
 })
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
+    $('#cep').mask('00000-000', {
+        placeholder: '012345-678'
 })
 
-$('form').validate({
+    $('form').validate({
     rules: {
         nome: {
-            requerido: true
+            required: true
         },
         email: {
-            requerido: true,
+            required: true,
             email: true
         },
         telefone: {
-            requerido: true
+            required: true
         },
         endereco: {
-            requerido: true
+            required: true
         },
         cep: {
-            requerido: true
+            required: true
         },
         cpf: {
-            requerido: true
+            required: true
         },
     },
     submitHandler: function (form) {
@@ -39,4 +45,5 @@ $('form').validate({
     invalidHandler: function (form, validator) {
         alert("Por favor, preencha os campos para prosseguir com a compra!");
     }
-})
+        })
+    })
